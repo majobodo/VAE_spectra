795bb67b4cfbaf729dd9ba09f4b8259def693721
majobodo <m.bonse@gmx.de>
2018-10-07 19:01:23 +0200

    Bug fix tensor size 2

--- VAEModel.py
+++ VAEModel.py
@@ -66,10 +66,10 @@ class ToTensor(object):
         spectrum, spectrum_ivar = sample['spectra'], sample['spectra_ivar']
         label, label_ivar = sample['labels'], sample['label_ivar']
 
-        return {'spectra': torch.from_numpy(spectrum).view(1, 1, -1),
-                'spectra_ivar': torch.from_numpy(spectrum_ivar).view(1, 1, -1),
-                'labels': torch.from_numpy(label).view(1, -1),
-                'label_ivar': torch.from_numpy(label_ivar).view(1, -1)}
+        return {'spectra': torch.from_numpy(spectrum).view(1, -1),
+                'spectra_ivar': torch.from_numpy(spectrum_ivar).view(1, -1),
+                'labels': torch.from_numpy(label).view(-1),
+                'label_ivar': torch.from_numpy(label_ivar).view(-1)}
 
 
 class VAEModel(SpectralModel):
